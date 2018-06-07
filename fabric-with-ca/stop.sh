a027f40a2b4262eebd58ee29a6ddd81480378521
nange <cctonange@gmail.com>
2018-06-07 15:40:27 +0800

    update

--- fabric-with-ca/stop.sh
+++ fabric-with-ca/stop.sh
@@ -9,4 +9,4 @@ docker-compose -f ./docker-compose-ca.yaml down
 # Stop chaincode containers and images as well
 docker rm -f $(docker ps -aq --filter name=dev-peer)
 docker rmi $(docker images | awk '$1 ~ /dev-peer/ { print $3 }')
-log "Docker containers have been stopped"
+echo "Docker containers have been stopped"
